7112440c19dd039c2af9929cd873bcf8c3f867d7
rob212 <robertmcbryde83@gmail.com>
2020-03-02 23:20:29 +0000

    addition of axios and simple test http call out to API to retrieve a list of recipes

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,14 @@
 // Global app controller
+import axios from 'axios';
+
+async function getResult(query) {
+    try{
+        const response = await axios(`https://forkify-api.herokuapp.com/api/search?q=${query}`);
+        const recipes = response.data.recipes
+        console.log(recipes);
+    } catch(error) {
+        alert(`Error occured while retrieveing recipes: ${error}`);
+    }
+}
+
+getResult('pizza');
