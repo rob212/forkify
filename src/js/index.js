e0d1e33c0e58991b425cb4d64c18702e87b3567f
rob212 <robertmcbryde83@gmail.com>
2020-03-09 21:28:07 +0000

    addition of state search data and a new event listener for the search form in the controller

--- src/js/index.js
+++ src/js/index.js
@@ -8,7 +8,27 @@ import Search from './models/Search';
  * - Shopping list object
  * - Liked recipes
  */
-const state = {}
+const state = {};
 
-const search = new Search('pizza');
-console.log(search.getResult());
+const controlSearch = async () => {
+    // 1) get the query from the view
+    const query = 'pizza' //Todo
+
+    if (query) {
+        // 2) New search object and add it to state
+        state.search = new Search(query);
+    } 
+
+    // 3) Prepare the UI for results
+
+    // 4) Search for recipes
+    await state.search.getResult();
+
+    // 5) render the results on the UI
+    console.log(state.search.result);
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
