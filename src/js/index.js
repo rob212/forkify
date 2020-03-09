3ebf203a3c0927c1b7112676c5affc81f2963f51
rob212 <robertmcbryde83@gmail.com>
2020-03-09 23:11:50 +0000

    addition of elements util in the base.js file and logic in controller to update the UI and state of app

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 // Global app controller
 import Search from './models/Search';
+import { elements } from './views/base';
+import * as searchView from './views/searchView';
 
 /**
  * Global state of the app
@@ -12,7 +14,7 @@ const state = {};
 
 const controlSearch = async () => {
     // 1) get the query from the view
-    const query = 'pizza' //Todo
+    const query =  searchView.getInput();
 
     if (query) {
         // 2) New search object and add it to state
@@ -20,15 +22,17 @@ const controlSearch = async () => {
     } 
 
     // 3) Prepare the UI for results
+    searchView.clearInput();
+    searchView.clearPreviousResults();
 
     // 4) Search for recipes
     await state.search.getResult();
 
     // 5) render the results on the UI
-    console.log(state.search.result);
+    searchView.renderResults(state.search.result);
 }
 
-document.querySelector('.search').addEventListener('submit', e => {
+elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
 });
